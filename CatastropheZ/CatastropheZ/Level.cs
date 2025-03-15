310ae5c90d8abf80157215db2bfbc10af1ece93c
Daniel <uhh@uhhhhh.huh>
2025-03-14 20:46:29 -0500

    fixed

--- CatastropheZ/CatastropheZ/Level.cs
+++ CatastropheZ/CatastropheZ/Level.cs
@@ -43,6 +43,7 @@ namespace CatastropheZ
                             xCount++;
                         }
                         yCount++;
+                        xCount = 0;
                     }
                 }
             }
